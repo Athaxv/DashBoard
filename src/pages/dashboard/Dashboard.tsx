0e985a70ced29134d1b93202aa123e07f13c9927
AtharvGaur <atharava112@gmail.com>
2025-01-07 14:59:50 +0530

    SideMenu

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,25 @@
+import { Box, Grid, Paper } from '@mui/material'
 import React from 'react'
+import scss from './Dashboard.module.scss'
 
 function Dashboard() {
   return (
-    <div>
-      <ul>
-        <li>Analytics</li>
-        <li>Profile</li>
-        <li>Settings</li>
-      </ul>
-    </div>
+    <Box>
+      <Grid container gap={2} className={scss.topCardsContainer}>
+        <Grid>
+          <Paper className={scss.dataCard} >xs=4</Paper>
+        </Grid>
+        <Grid>
+          <Paper className={scss.dataCard} >xs=4</Paper>
+        </Grid>
+        <Grid>
+          <Paper className={scss.dataCard} >xs=4</Paper>
+        </Grid>
+      </Grid>
+      <Grid xs={12} marginY={2}>
+          <Paper className={scss.dataCard} >xs=8</Paper>
+        </Grid>
+    </Box>
   )
 }
 
